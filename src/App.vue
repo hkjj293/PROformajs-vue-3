a72530b81ceec23a99ca3465f52ab6462732915a
hkjj293 <chrishuihuihui@gmail.com>
2023-03-13 16:52:16 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,110 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import Vue from 'vue';
+import { Protocol } from '@openclinical/proformajs';
+
+const template = {
+  "class": "Plan",
+  "name": "plan",
+  "caption": "Plan",
+  "dataDefinitions": [],
+  "tasks": [],
+  "autonomous": true,
+  "meta": {
+    "svg": {
+      "width": 800,
+      "height": 400
+    }
+  }
+};
+
+export default Vue.extend({
+  name: 'ServeDev',
+  data: function () {
+    return {
+      selectedtask: template.name, // initial value,
+      tab: "compose",
+      protocol: new Protocol.Plan(template),
+      initialData: {}
+    };
+  },
+  created: function () {
+    // initial data can be passed in the querystring
+    // see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+    const urlParams = new URLSearchParams(window.location.search);
+    for (let entry of urlParams.entries()) {
+      this.initialData[entry[0]] = JSON.parse(entry[1]);
+    }
+  },
+  computed: {
+    startData() {
+      let result = {};
+      if (this.initialData && Object.keys(this.initialData).length > 0) {
+        result[this.protocol.name] = this.initialData;
+      }
+      return result;
+    }
+  },
+  methods: {
+    updateProtocol(e) {
+      this.protocol = e.value;
+      if (e.selected) {
+        this.selectedtask = e.selected;
+      }
+    },
+    resetProtocol(clazz) {
+      if (clazz && ["Action", "Decision", "Enquiry"].indexOf(clazz) > -1) {
+        let proto = new Protocol[clazz]({ name: clazz.toLowerCase(), caption: clazz, meta: { svg: { width: 400, height: 200 }, pos: { x: 190, y: 100 } } });
+        this.protocol = proto;
+        this.selectedtask = clazz.toLowerCase();
+      } else {
+        this.protocol = new Protocol.Plan(template);
+        this.selectedtask = template.name;
+      }
+    },
+    updateSelectedTask(evt) {
+      this.selectedtask = evt.value;
+    },
+  },
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <main role="main">
+      <b-navbar toggleable="lg" variant="primary" type="dark" fixed>
+        <div class="container-fluid">
+          <b-navbar-brand>PRO<em>formajs</em></b-navbar-brand>
+          <b-navbar-toggle target="nav_collapse" />
+          <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown text="Reset" right>
+                <b-dropdown-item @click="resetProtocol">Plan</b-dropdown-item>
+                <b-dropdown-item @click="resetProtocol('Decision')">Decision</b-dropdown-item>
+                <b-dropdown-item @click="resetProtocol('Action')">Action</b-dropdown-item>
+                <b-dropdown-item @click="resetProtocol('Enquiry')">Enquiry</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
+      </b-navbar>
+      <div class="container-fluid">
+        <b-tabs content-class="mt-3" class="mt-3">
+          <!-- <b-tab title="Compose" active> -->
+          <!-- <p-compose :protocol="protocol" :selectedtask="selectedtask" @change-protocol="updateProtocol" -->
+          <!-- @select-task="updateSelectedTask" /> -->
+          <!-- </b-tab> -->
+          <b-tab title="Review" :disabled="!protocol || !protocol.isValid()">
+            <pc-review :protocol="protocol" :debug="true" :initialData="startData"
+              :template="protocol && protocol.meta && protocol.meta.enact && protocol.meta.enact.template ? protocol.meta.enact.template : 'compact'" />
+          </b-tab>
+        </b-tabs>
+      </div>
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+.clickable {
+  cursor: pointer;
 }
 </style>
---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+// /* NB removed :: eslint-disable import/prefer-default-export */
+// import Compose from './Compose.vue'
+// import ComposeTask from './ComposeTask.vue'
+// import ComposeTree from './ComposeTree.vue'
+// import ComposeCode from './ComposeCode.vue'
+// import ComposeReview from './ComposeReview.vue'
+// import ComposeMap from './ComposeMap.vue'
+// import ReviewMarkdown from './ReviewMarkdown.vue'
+
+// export {
+//     Compose,
+//     ComposeMap,
+//     ComposeTask,
+//     ComposeTree,
+//     ComposeCode,
+//     ComposeReview,
+//     ReviewMarkdown
+// }
