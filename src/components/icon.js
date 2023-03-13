99da7acb17e030d5baef5ee66babdbce85030ef4
hkjj293 <chrishuihuihui@gmail.com>
2023-03-13 16:13:31 +0000

    added icon.js

---
+++ src/components/icon.js
@@ -0,0 +1,13 @@
+export const IconMixin = {
+  props: {
+    scale: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    transform() {
+      return this.scale!==1 ? "scale(" + this.scale + ", " + this.scale + ")" : "";
+    }
+  }
+}
