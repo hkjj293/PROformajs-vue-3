beaf8f3d1e0f3e9e7cb4eaa1c7ce9dd4d3c63436
hkjj293 <chrishuihuihui@gmail.com>
2023-03-13 16:13:01 +0000

    added map.js

---
+++ src/components/map.js
@@ -0,0 +1,9 @@
+export const task_colors = {
+  plan: { todo: '#FFBF00', done: '#F5DA81' },
+  enquiry: { todo: '#00FF40', done: '#A9F5BC' },
+  decision: { todo: '#FA58F4', done: '#F5A9F2' },
+  task: { todo: '#FEED2E', done: '#F5F6CE' },
+  action: { todo: '#3BD2FF', done: '#C4F1FF' }
+};
+
+export const discarded_color = '#DDD';
