ebe41189ce0b339879271025e7c3d209c472356c
hkjj293 <chrishuihuihui@gmail.com>
2023-03-13 16:13:56 +0000

    added attribute,js

---
+++ src/components/attribute.js
@@ -0,0 +1,22 @@
+// some shared code for most of the Compose{Input}.vue components (though not ComposeName.vue)
+
+export const AttributeMixin = {
+  props: ['att', 'comp', 'label', 'description', 'disabled'],
+  computed: {
+    labelValue: function () {
+      // return "de camel cased" name if label not provided
+      // first clause adds space before each capital
+      // second clause upper cases the first character
+      return this.label || this.att.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); });
+    }
+  }
+}
+
+export const TypeMixin = {
+  props: ['type'],
+  computed: {
+    typeValue: function() {
+      return this.type || 'text';
+    }
+  }
+}
