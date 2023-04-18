7bd73096678030a0097564c27c828ada98c7916e
hkjj293 <chrishuihuihui@gmail.com>
2023-04-18 16:28:55 +0100

    Solved #82

--- src/tool-components/PopoverButton.vue
+++ src/tool-components/PopoverButton.vue
@@ -10,7 +10,8 @@ export default {
       required: true
     },
     targetId: {
-      type: String
+      type: String,
+      default: null
     },
     varient: {
       type: String,
@@ -50,22 +51,17 @@ export default {
     return { popover: null, message: this.msg }
   },
   mounted() {
+    this.onClick.bind(this)
     this.popover = new Popover(this.$el, { sanitize: false })
-    if (this.targetId && document.getElementById(this.targetId)) {
-      this.message = document.getElementById(this.targetId).innerHTML
+    this.onShow()
+    if (this.targetId) {
+      document.addEventListener('click', this.onClick);
     }
-    let content = {
-      '.popover-body': this.message,
-      '.popover-header': this.title
-    }
-    if (this.noTitle) {
-      content = {
-        '.popover-body': this.message
-      }
-    }
-    this.popover.setContent(content)
     this.$el.addEventListener('shown.bs.popover', this.onShown)
     this.$el.addEventListener('show.bs.popover', this.onShow)
+  },
+  beforeUnmount() {
+    document.removeEventListener('click', this.onClick)
   },
   methods: {
     onShown() {
@@ -85,6 +81,13 @@ export default {
         }
       }
       this.popover.setContent(content)
+    },
+    onClick: function (event) {
+      if (this.targetId &&
+        this.popover.tip &&
+        !(document.getElementById(this.popover.tip.id).contains(event.target))) {
+        this.popover.hide();
+      }
     }
   }
 }
